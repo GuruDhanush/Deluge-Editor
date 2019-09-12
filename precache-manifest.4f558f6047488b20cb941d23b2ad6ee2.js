529e990505e2a2b2071fb8358a2a3369c9da7256
GuruDhanush <gurudhanush@outlook.com>
2019-09-12 20:48:57 +0530

    Updates

---
+++ precache-manifest.4f558f6047488b20cb941d23b2ad6ee2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a8f6d7dc4c8cf2a201",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "48a8f6d7dc4c8cf2a201",
+    "url": "app.css"
+  },
+  {
+    "revision": "8fcc53c3d24100091b9f",
+    "url": "editor.worker.bundle.js"
+  },
+  {
+    "revision": "3842fc833200dce7154aa4fda0234921",
+    "url": "icon.png"
+  },
+  {
+    "url": "icon_192x192.73d036e77848a1e07f93c63cf003cb54.png"
+  },
+  {
+    "url": "icon_256x256.fc06ec9d97fff7d9d9780a15f444466a.png"
+  },
+  {
+    "url": "icon_48x48.3d836617c84979de0939575ce0200db4.png"
+  },
+  {
+    "revision": "c49a7b9e9016e4bf529d75b5386a180f",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.5cf1ed49ef86c0ef5561a655a56cbf4e.json"
+  }
+]);
